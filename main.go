4d33cded96b4f012b6b88df7fc90802bc70a8d6a
Duc Tran <trannhuduc531998@gmail.com>
2021-10-16 17:58:11 +0700

    Rm flag

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"fano/internal/fano"
 	"flag"
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/cli"
@@ -18,29 +16,14 @@ func init() {
 	log.SetLevel(log.WarnLevel)
 }
 
-type arrayFlags []string
-
-func (i *arrayFlags) String() string {
-	return strings.Join(*i, ", ")
+func fano_output(name, val string) {
+	fmt.Println(name, val)
 }
-
-func (i *arrayFlags) Set(value string) error {
-	*i = append(*i, value)
-	return nil
-}
-
-// Globs of files that should not be pruned
-var exclusionGlobs arrayFlags
-
-// Globs of files that should always be pruned in addition to the defaults
-var inclusionGlobs arrayFlags
 
 func main() {
 	debug := flag.Bool("verbose", false, "Verbose log output.")
 	flag.Parse()
 	dir := flag.Arg(0)
-
-	start := time.Now()
 
 	if *debug {
 		log.SetLevel(log.DebugLevel)
@@ -52,17 +35,9 @@ func main() {
 		options = append(options, fano.WithDir(dir))
 	}
 
-	if len(exclusionGlobs) > 0 {
-		options = append(options, fano.WithExceptions(exclusionGlobs))
-	}
-
-	if len(inclusionGlobs) > 0 {
-		options = append(options, fano.WithGlobs(inclusionGlobs))
-	}
-
 	p := fano.New(options...)
 
-	stats, err := p.Prune()
+	stats, err := p.Fano()
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
@@ -70,12 +45,7 @@ func main() {
 	println()
 	defer println()
 
-	output("Files total", humanize.Comma(stats.FilesTotal))
-	output("Files removed", humanize.Comma(stats.FilesRemoved))
-	output("Size removed", humanize.Bytes(uint64(stats.SizeRemoved)))
-	output("Duration", time.Since(start).Round(time.Millisecond).String())
-}
-
-func output(name, val string) {
-	fmt.Printf("\x1b[1m%20s\x1b[0m %s\n", name, val)
+	fano_output("Files total:   ", humanize.Comma(stats.FilesTotal))
+	fano_output("Files removed: ", humanize.Comma(stats.FilesRemoved))
+	fano_output("Size removed:  ", humanize.Bytes(uint64(stats.SizeRemoved)))
 }
